baf26dbc082033c8476b09261dce6e5d431b3c35
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-13 23:21:13 +0100

    Add form for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: 'form.title', body: 'check')
+
+    if @article.save
+      redirect_to @article, flash[:success] = "Post added successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
